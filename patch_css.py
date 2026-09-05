5ada11fd5d5343ff5f88cbb138eb901bd158ec2d
Nav44n <naveenj1104@gmail.com>
2026-09-05 20:23:01 +0530

    Chore: Remove temporary patch scripts

--- patch_css.py
+++
@@ -1,8 +0,0 @@
-with open('style.css', 'r', encoding='utf-8') as f:
-    css = f.read()
-
-css = css.replace('content: " ";', 'content: "\\u25bc";')
-css = css.replace('content: "?";', 'content: "\\u25bc";')
-
-with open('style.css', 'w', encoding='utf-8') as f:
-    f.write(css)
